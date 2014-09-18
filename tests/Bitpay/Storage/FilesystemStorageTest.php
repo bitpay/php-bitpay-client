21b4f890dfed18e5452704edb038d13697df5c9d
Joshua Estes <Joshua@dSpaceLabs.com>
2014-09-18 14:11:22 -0400

    Added test for not readable on filesystem

--- tests/Bitpay/Storage/FilesystemStorageTest.php
+++ tests/Bitpay/Storage/FilesystemStorageTest.php
@@ -46,6 +46,7 @@ class FilesystemStorageTest extends \PHPUnit_Framework_TestCase
     public function testLoad()
     {
         $storage = new FilesystemStorage();
+
         vfsStream::newFile('public.key')
             ->at($this->root)
             ->setContent('C:16:"Bitpay\PublicKey":62:{a:5:{i:0;s:20:"vfs://tmp/public.key";i:1;N;i:2;N;i:3;N;i:4;N;}}');
@@ -60,6 +61,20 @@ class FilesystemStorageTest extends \PHPUnit_Framework_TestCase
     public function testNotFileException()
     {
         $storage = new FilesystemStorage();
+        $storage->load(vfsStream::url('tmp/public.key'));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testLoadNotReadableException()
+    {
+        $storage = new FilesystemStorage();
+        vfsStream::newFile('public.key', 0600)
+            ->at($this->root)
+            ->setContent('C:16:"Bitpay\PublicKey":62:{a:5:{i:0;s:20:"vfs://tmp/public.key";i:1;N;i:2;N;i:3;N;i:4;N;}}')
+            ->chown(vfsStream::OWNER_ROOT)
+            ->chgrp(vfsStream::GROUP_ROOT);
         $storage->load(vfsStream::url('tmp/public.key'));
     }
 }
