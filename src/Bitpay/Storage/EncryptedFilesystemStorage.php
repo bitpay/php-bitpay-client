b9ee2eed115db592e3f8c128f7998b09e8e0af1b
Joshua Estes <Joshua@dSpaceLabs.com>
2014-09-18 15:21:26 -0400

    Working on allowing keys to be saved on filesystem using a password

---
+++ src/Bitpay/Storage/EncryptedFilesystemStorage.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 BitPay, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Bitpay\Storage;
+
+/**
+ */
+class EncryptedFilesystemStorage implements StorageInterface
+{
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Initialization Vector
+     */
+    const IV = '0000000000000000';
+
+    /**
+     * @var string
+     */
+    const METHOD = 'AES-128-CBC';
+
+    /**
+     * @param string $password
+     */
+    public function __construct($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function persist(\Bitpay\KeyInterface $key)
+    {
+        $path    = $key->getId();
+        $data    = serialize($key);
+        $encoded = bin2hex(openssl_encrypt(
+            $data,
+            self::METHOD,
+            $this->password,
+            OPENSSL_RAW_DATA,
+            self::IV
+        ));
+
+        file_put_contents($path, $encoded);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function load($id)
+    {
+        if (!is_file($id)) {
+            throw new \Exception(sprintf('Could not find "%s"', $id));
+        }
+
+        if (!is_readable($id)) {
+            throw new \Exception(sprintf('"%s" cannot be read, check permissions', $id));
+        }
+
+        $encoded = file_get_contents($id);
+        $decoded = openssl_decrypt(
+            hex2bin($encoded),
+            self::METHOD,
+            $this->password,
+            OPENSSL_RAW_DATA,
+            self::IV
+        );
+
+        if (false === $decoded) {
+            throw new \Exception('Could not decode key');
+        }
+
+        return unserialize($decoded);
+    }
+}
---
+++ tests/Bitpay/Storage/EncryptedFilesystemStorageTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 BitPay, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Bitpay\Storage;
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\visitor\vfsStreamStructureVisitor;
+
+class EncryptedFilesystemStorageTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->root = vfsStream::setup('tmp');
+    }
+
+    public function testPersist()
+    {
+        $storage = new EncryptedFilesystemStorage('satoshi');
+        $storage->persist(new \Bitpay\PublicKey(vfsStream::url('tmp/public.key')));
+        $this->assertTrue($this->root->hasChild('tmp/public.key'));
+    }
+
+    public function testLoad()
+    {
+        $storage = new EncryptedFilesystemStorage('satoshi');
+
+        vfsStream::newFile('public.key')
+            ->at($this->root)
+            ->setContent('8bc03b8e4272d47ea81d63c6571b8172072ed03203ff7cd3fd434c03f7994b5721363d0dda3cec833f6f263bde0ececa06b79f68d5616be18b8e9311c486223e18c7424daaa59991f4b10db9f2fb8b4c42896c50d216010b403d562738ef5a96');
+
+        $key = $storage->load(vfsStream::url('tmp/public.key'));
+        $this->assertInstanceOf('Bitpay\PublicKey', $key);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testNotFileException()
+    {
+        $storage = new EncryptedFilesystemStorage('satoshi');
+        $storage->load(vfsStream::url('tmp/public.key'));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testLoadNotReadableException()
+    {
+        $storage = new EncryptedFilesystemStorage('satoshi');
+        vfsStream::newFile('public.key', 0600)
+            ->at($this->root)
+            ->setContent('')
+            ->chown(vfsStream::OWNER_ROOT)
+            ->chgrp(vfsStream::GROUP_ROOT);
+        $storage->load(vfsStream::url('tmp/public.key'));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testLoadCouldNotDecode()
+    {
+        $storage = new EncryptedFilesystemStorage('satoshi');
+
+        vfsStream::newFile('public.key')
+            ->at($this->root)
+            ->setContent('00');
+
+        $key = $storage->load(vfsStream::url('tmp/public.key'));
+        $this->assertInstanceOf('Bitpay\PublicKey', $key);
+    }
+}
