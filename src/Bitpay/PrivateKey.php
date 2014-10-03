701f6679f6ae02b92ec77369b00d1300ae99d7e6
ionux <rich@bitpay.com>
2014-10-03 15:46:25 -0400

    Added new PEM encoding and decoding functions. Also added two corresponding tests.

--- src/Bitpay/PrivateKey.php
+++ src/Bitpay/PrivateKey.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License 
+ * @license Copyright 2011-2014 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -26,6 +26,16 @@ class PrivateKey extends Key
      * @var boolean
      */
     protected $generated = false;
+
+    /**
+     * @var string
+     */
+    public $pemEncoded = '';
+
+    /**
+     * @var array
+     */
+    public $pemDecoded = array();
 
     /**
      * @return string
@@ -311,5 +321,125 @@ class PrivateKey extends Key
         $retval['seq'] = bin2hex($seq);
 
         return $retval;
+    }
+
+    /**
+     * Decodes PEM data to retrieve the keypair.
+     *
+     * @param  string $pem_data The data to decode.
+     * @return array            The keypair info.
+     */
+    private function pemDecode($pem_data)
+    {
+        $beg_ec_text = '-----BEGIN EC PRIVATE KEY-----';
+        $end_ec_text = '-----END EC PRIVATE KEY-----';
+
+        $decoded = '';
+
+        $ecpemstruct = array();
+
+        $pem_data = str_ireplace($beg_ec_text, '', $pem_data);
+        $pem_data = str_ireplace($end_ec_text, '', $pem_data);
+        $pem_data = str_ireplace("\r", '', trim($pem_data));
+        $pem_data = str_ireplace("\n", '', trim($pem_data));
+        $pem_data = str_ireplace(' ',  '', trim($pem_data));
+
+        $decoded = bin2hex(base64_decode($pem_data));
+
+        if (strlen($decoded) < 230) {
+            throw new \Exception('Invalid or corrupt secp256k1 key provided. Cannot decode the supplied PEM data.');
+        }
+
+        $ecpemstruct = array(
+                'oct_sec_val'  => substr($decoded,14,64),
+                'obj_id_val'   => substr($decoded,86,10),
+                'bit_str_val'  => substr($decoded,106),
+        );
+
+        if ($ecpemstruct['obj_id_val'] != '2b8104000a') {
+            throw new \Exception('Invalid or corrupt secp256k1 key provided. Cannot decode the supplied PEM data.');
+        }
+
+        $private_key = $ecpemstruct['oct_sec_val'];
+        $public_key  = $ecpemstruct['bit_str_val'];
+
+        if (strlen($private_key) < 64 || strlen($public_key) < 128) {
+            throw new \Exception('Invalid or corrupt secp256k1 key provided. Cannot decode the supplied PEM data.');
+        }
+
+        $this->pemDecoded = array('private_key' => $private_key, 'public_key' => $public_key);
+
+        return $this->pemDecoded;
+    }
+
+    /**
+     * Encodes keypair data to PEM format.
+     *
+     * @param  array  $keypair The keypair info.
+     * @return string          The data to decode.
+     */
+    public function pemEncode($keypair)
+    {
+        if (is_array($keypair) && (strlen($keypair[0]) < 64 || strlen($keypair[1]) < 128)) {
+            throw new \Exception('Invalid or corrupt secp256k1 keypair provided. Cannot decode the supplied PEM data.');
+        }
+
+        $dec         = '';
+        $byte        = '';
+        $seq         = '';
+        $decoded     = '';
+        $beg_ec_text = '';
+        $end_ec_text = '';
+        $ecpemstruct = array();
+        $digits      = array();
+
+        for ($x = 0; $x < 256; $x++) {
+            $digits[$x] = chr($x);
+        }
+
+        $ecpemstruct = array(
+                'sequence_beg' => '30',
+                'total_len'    => '74',
+                'int_sec_beg'  => '02',
+                'int_sec_len'  => '01',
+                'int_sec_val'  => '01',
+                'oct_sec_beg'  => '04',
+                'oct_sec_len'  => '20',
+                'oct_sec_val'  => $keypair[0],
+                'a0_ele_beg'   => 'a0',
+                'a0_ele_len'   => '07',
+                'obj_id_beg'   => '06',
+                'obj_id_len'   => '05',
+                'obj_id_val'   => '2b8104000a',
+                'a1_ele_beg'   => 'a1',
+                'a1_ele_len'   => '44',
+                'bit_str_beg'  => '03',
+                'bit_str_len'  => '42',
+                'bit_str_val'  => '00'.$keypair[1],
+        );
+
+        $beg_ec_text = '-----BEGIN EC PRIVATE KEY-----';
+        $end_ec_text = '-----END EC PRIVATE KEY-----';
+
+        $dec = trim(implode($ecpemstruct));
+
+        if (strlen($dec) < 230) {
+            throw new \Exception('Invalid or corrupt secp256k1 keypair provided. Cannot encode the supplied data.');
+        }
+
+        $dec = Util::decodeHex('0x'.$dec);
+
+        while (gmp_cmp($dec, '0') > 0) {
+            $dv = gmp_div($dec, '256');
+            $rem = gmp_strval(gmp_mod($dec, '256'));
+            $dec = $dv;
+            $byte = $byte.$digits[$rem];
+        }
+
+        $byte = $beg_ec_text . "\r\n" . chunk_splt(base64_encode(strrev($byte)), 64) . $end_ec_text;
+
+        $this->pemEncoded = $byte;
+
+        return $byte;
     }
 }
--- tests/Bitpay/PrivateKeyTest.php
+++ tests/Bitpay/PrivateKeyTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License 
+ * @license Copyright 2011-2014 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -152,5 +152,58 @@ class PrivateKeyTest extends \PHPUnit_Framework_TestCase
         $publicKey = $key->getPublicKey();
 
         $this->assertInstanceOf('Bitpay\PublicKey', $publicKey);
+    }
+
+    public function testPemDecode()
+    {
+        $data = '-----BEGIN EC PRIVATE KEY-----' . "\r\n" .
+                'MHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK' . "\r\n" .
+                'oUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX' . "\r\n" .
+                'fAArmNMaGotTpjdnymWlMfszzXJhlw==' . "\r\n" .
+                '-----END EC PRIVATE KEY-----';
+
+        $private_key = '283b13834de77624696b80c29a2a8df0287e3cca23b26ad5fe77d9d5fed0ee90';
+        $public_key  = '048d970d6ba29dcfa190c177140fd889fadd6d2590b1ee1a6a06e255dbf22b4017ee7bc8e1f07ed0ff8bd77c002b98d31a1a8b53a63767ca65a531fb33cd726197';
+
+        $pkey = new PrivateKey();
+        $this->assertNotNull($pkey);
+
+        $keys = $pkey->pemDecode($data);
+        $this->assertNotNull($pkey);
+
+        // Ensure it's an array
+        $this->assertInternalType('array', $keys);
+
+        // Ensure the private key matches the expected value
+        $this->assertEquals($private_key, $keys['private_key']);
+
+        // Ensure the public key matches the expected value
+        $this->assertEquals($public_key, $keys['public_key']);
+    }
+
+    public function testPemEncode()
+    {
+        $data = '-----BEGIN EC PRIVATE KEY-----' . "\r\n" .
+                'MHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK' . "\r\n" .
+                'oUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX' . "\r\n" .
+                'fAArmNMaGotTpjdnymWlMfszzXJhlw==' . "\r\n" .
+                '-----END EC PRIVATE KEY-----';
+
+        $private_key = '283b13834de77624696b80c29a2a8df0287e3cca23b26ad5fe77d9d5fed0ee90';
+        $public_key  = '048d970d6ba29dcfa190c177140fd889fadd6d2590b1ee1a6a06e255dbf22b4017ee7bc8e1f07ed0ff8bd77c002b98d31a1a8b53a63767ca65a531fb33cd726197';
+
+        $keypair = array($private_key, $public_key);
+
+        $pkey = new PrivateKey();
+        $this->assertNotNull($pkey);
+
+        $pemdata = $pkey->pemEncode($keypair);
+        $this->assertNotNull($pemdata);
+
+        // Ensure it's a string
+        $this->assertInternalType('string', $pemdata);
+
+        // Ensure the PEM-encoded data matches the expected value
+        $this->assertEquals($data, $pemdata);
     }
 }
