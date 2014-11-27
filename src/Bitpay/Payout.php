591f9c457a7eee29b8390fdc3ce22d7eb7a265ce
BitWasp <thomas@bitwasp.co>
2014-11-27 04:41:58 +0000

    Add tests

--- src/Bitpay/Payout.php
+++ src/Bitpay/Payout.php
@@ -33,6 +33,10 @@ class Payout implements PayoutInterface
     protected $amount;
 
     /**
+     * @var float
+     */
+    protected $btc;
+    /**
      * @var CurrencyInterface
      */
     protected $currency;
@@ -146,7 +150,7 @@ class Payout implements PayoutInterface
      * @param $amount
      * @return $this
      */
-    private function setAmount($amount)
+    public function setAmount($amount)
     {
         if (!empty($amount)) {
             $this->amount = $amount;
@@ -281,8 +285,6 @@ class Payout implements PayoutInterface
     {
         if (!empty($instruction)) {
             $this->instructions[] = $instruction;
-            $new_total = $this->getAmount() + $instruction->getAmount();
-            $this->setAmount($new_total);
         }
 
         return $this;
