c1530ce7d91d4b8e62456034e5eae5e09403116e
BitWasp <thomas@bitwasp.co>
2014-11-27 03:23:26 +0000

    Add function to set a bitcoin amount, as returned by Bitpay.

--- src/Bitpay/Payout.php
+++ src/Bitpay/Payout.php
@@ -224,6 +224,28 @@ class Payout implements PayoutInterface
     /**
      * @inheritdoc
      */
+    public function getBtcAmount()
+    {
+        return $this->btc;
+    }
+
+    /**
+     * Set the Bitcoin amount for this payout, once set by Bitpay.
+     * @param $amount
+     * @return $this
+     */
+    public function setBtcAmount($amount)
+    {
+        if (!empty($amount)) {
+            $this->btc = $amount;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function getRequestDate()
     {
         return $this->requestDate;
