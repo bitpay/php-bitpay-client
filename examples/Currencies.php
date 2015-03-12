64cce14c8a64abd3a4fd04b4a50e82e8c3f82a57
Rich Morgan <ionux@users.noreply.github.com>
2015-03-12 18:10:06 -0400

    Added copyright year notice

--- examples/Currencies.php
+++ examples/Currencies.php
@@ -1,9 +1,11 @@
 <?php
+/**
+ * Copyright (c) 2014-2015 BitPay
+ */
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$bitpay = new \Bitpay\Bitpay();
-
+$bitpay     = new \Bitpay\Bitpay();
 $client     = $bitpay->get('client');
 $currencies = $client->getCurrencies();
 
