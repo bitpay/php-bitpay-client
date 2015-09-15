e6aa1c44b2534d0858209402c811ed2ff308893c
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 18:47:56 -0400

    Updated copyright year and minor refactoring

--- tests/Bitpay/Math/GmpEngineTest.php
+++ tests/Bitpay/Math/GmpEngineTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -8,119 +8,96 @@ namespace Bitpay\Math;
 
 class GmpEngineTest extends \PHPUnit_Framework_TestCase
 {
+    private $a;
+    private $b;
+    private $c;
+    private $math;
+
     /**
      * @requires  extension gmp
      */
     protected function setUp()
     {
-      if (!extension_loaded('gmp'))
-      {
-        $this->markTestSkipped('The GMP extension is NOT loaded! You must enable it to run this test');
-      }
+        $this->a = 1234;
+        $this->b = '1234123412341234123412341234123412412341234213412421341342342';
+        $this->c = '0x1234123412341234123412341234123412412341234213412421341342342';
+
+        if (!extension_loaded('gmp')) {
+            $this->markTestSkipped('The GMP extension is NOT loaded! You must enable it to run this test');
+        } else {
+            $this->math = new GmpEngine();
+        }
     }
 
-   	public function testadd()
+    public function testAdd()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_add($a, $a)), $math->add($a, $a));
-        $this->assertEquals(gmp_strval(gmp_add($b, $b)), $math->add($b, $b));
-        $this->assertEquals(gmp_strval(gmp_add($c, $c)), $math->add($c, $c));
-        $this->assertEquals(2, $math->add(1, 1));
+        $this->assertEquals(gmp_strval(gmp_add($this->a, $this->a)), $this->math->add($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_add($this->b, $this->b)), $this->math->add($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_add($this->c, $this->c)), $this->math->add($this->c, $this->c));
+        $this->assertEquals(2, $this->math->add(1, 1));
     }
 
-    public function testcmp()
+    public function testCmp()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_cmp($a, $a)), $math->cmp($a, $a));
-        $this->assertEquals(gmp_strval(gmp_cmp($b, $b)), $math->cmp($b, $b));
-        $this->assertEquals(gmp_strval(gmp_cmp($c, $c)), $math->cmp($c, $c));
-        $this->assertEquals(0, $math->cmp(1, 1));
+        $this->assertEquals(gmp_strval(gmp_cmp($this->a, $this->a)), $this->math->cmp($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_cmp($this->b, $this->b)), $this->math->cmp($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_cmp($this->c, $this->c)), $this->math->cmp($this->c, $this->c));
+        $this->assertEquals(0, $this->math->cmp(1, 1));
     }
 
-    public function testdiv()
+    public function testDiv()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_div($a, $a)), $math->div($a, $a));
-        $this->assertEquals(gmp_strval(gmp_div($b, $b)), $math->div($b, $b));
-        $this->assertEquals(gmp_strval(gmp_div($c, $c)), $math->div($c, $c));
-        $this->assertEquals(1, $math->div(1, 1));
+        $this->assertEquals(gmp_strval(gmp_div($this->a, $this->a)), $this->math->div($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_div($this->b, $this->b)), $this->math->div($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_div($this->c, $this->c)), $this->math->div($this->c, $this->c));
+        $this->assertEquals(1, $this->math->div(1, 1));
     }
 
-    public function testinvertm()
+    public function testInvertm()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_invert($a, $a)), $math->invertm($a, $a));
-        $this->assertEquals(gmp_strval(gmp_invert($b, $b)), $math->invertm($b, $b));
-        $this->assertEquals(gmp_strval(gmp_invert($c, $c)), $math->invertm($c, $c));
-        $this->assertEquals(0, $math->invertm(1, 1));
+        $this->assertEquals(gmp_strval(gmp_invert($this->a, $this->a)), $this->math->invertm($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_invert($this->b, $this->b)), $this->math->invertm($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_invert($this->c, $this->c)), $this->math->invertm($this->c, $this->c));
+        $this->assertEquals(0, $this->math->invertm(1, 1));
 
         $o = '2';
         $p = '0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f';
-        $this->assertEquals('57896044618658097711785492504343953926634992332820282019728792003954417335832', $math->invertm($o, $p));
+        $this->assertEquals('57896044618658097711785492504343953926634992332820282019728792003954417335832', $this->math->invertm($o, $p));
 
         $o = '-207267379875244730201206352791949018434229233557197871725317424106240926035466';
         $p = '0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f';
-        $this->assertEquals('93736451599995461267424215486556527005103980679329099329644578865571485201981', $math->invertm($o, $p));
+        $this->assertEquals('93736451599995461267424215486556527005103980679329099329644578865571485201981', $this->math->invertm($o, $p));
     }
 
-    public function testmod()
+    public function testMod()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_mod($a, $a)), $math->mod($a, $a));
-        $this->assertEquals(gmp_strval(gmp_mod($b, $b)), $math->mod($b, $b));
-        $this->assertEquals(gmp_strval(gmp_mod($c, $c)), $math->mod($c, $c));
-        $this->assertEquals(0, $math->mod(1, 1));
+        $this->assertEquals(gmp_strval(gmp_mod($this->a, $this->a)), $this->math->mod($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_mod($this->b, $this->b)), $this->math->mod($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_mod($this->c, $this->c)), $this->math->mod($this->c, $this->c));
+        $this->assertEquals(0, $this->math->mod(1, 1));
     }
 
-    public function testmul()
+    public function testMul()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_mul($a, $a)), $math->mul($a, $a));
-        $this->assertEquals(gmp_strval(gmp_mul($b, $b)), $math->mul($b, $b));
-        $this->assertEquals(gmp_strval(gmp_mul($c, $c)), $math->mul($c, $c));
-        $this->assertEquals(1, $math->mul(1, 1));
+        $this->assertEquals(gmp_strval(gmp_mul($this->a, $this->a)), $this->math->mul($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_mul($this->b, $this->b)), $this->math->mul($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_mul($this->c, $this->c)), $this->math->mul($this->c, $this->c));
+        $this->assertEquals(1, $this->math->mul(1, 1));
     }
 
-    public function testpow()
+    public function testPow()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_pow($a, $a)), $math->pow($a, $a));
-        $this->assertEquals(gmp_strval(gmp_pow($b, $b)), $math->pow($b, $b));
-        $this->assertEquals(gmp_strval(gmp_pow($c, $c)), $math->pow($c, $c));
-        $this->assertEquals(1, $math->pow(1, 1));
+        $this->assertEquals(gmp_strval(gmp_pow($this->a, $this->a)), $this->math->pow($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_pow($this->b, $this->b)), $this->math->pow($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_pow($this->c, $this->c)), $this->math->pow($this->c, $this->c));
+        $this->assertEquals(1, $this->math->pow(1, 1));
     }
 
-    public function testsub()
+    public function testSub()
     {
-        $a = 1234;
-        $b = '1234123412341234123412341234123412412341234213412421341342342';
-        $c = '0x1234123412341234123412341234123412412341234213412421341342342';
-        $math = new GmpEngine();
-        $this->assertEquals(gmp_strval(gmp_sub($a, $a)), $math->sub($a, $a));
-        $this->assertEquals(gmp_strval(gmp_sub($b, $b)), $math->sub($b, $b));
-        $this->assertEquals(gmp_strval(gmp_sub($c, $c)), $math->sub($c, $c));
-        $this->assertEquals(0, $math->sub(1, 1));
+        $this->assertEquals(gmp_strval(gmp_sub($this->a, $this->a)), $this->math->sub($this->a, $this->a));
+        $this->assertEquals(gmp_strval(gmp_sub($this->b, $this->b)), $this->math->sub($this->b, $this->b));
+        $this->assertEquals(gmp_strval(gmp_sub($this->c, $this->c)), $this->math->sub($this->c, $this->c));
+        $this->assertEquals(0, $this->math->sub(1, 1));
     }
 }
