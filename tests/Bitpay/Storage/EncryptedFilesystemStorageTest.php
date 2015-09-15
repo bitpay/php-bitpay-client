effcee7a15993dca443c1b81c737827fd8fdfc54
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 18:28:12 -0400

    Updated copyright year and minor refactoring

--- tests/Bitpay/Storage/EncryptedFilesystemStorageTest.php
+++ tests/Bitpay/Storage/EncryptedFilesystemStorageTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -10,6 +10,8 @@ use org\bovigo\vfs\vfsStream;
 
 class EncryptedFilesystemStorageTest extends \PHPUnit_Framework_TestCase
 {
+    private $root;
+
     public function setUp()
     {
         $this->root = vfsStream::setup('tmp');
