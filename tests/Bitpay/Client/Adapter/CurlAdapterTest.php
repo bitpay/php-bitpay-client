e211ed5834a85f1e045752497a1c781b8f0aea4b
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 19:02:41 -0400

    Updated copyright year and minor refactoring

--- tests/Bitpay/Client/Adapter/CurlAdapterTest.php
+++ tests/Bitpay/Client/Adapter/CurlAdapterTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -55,5 +55,4 @@ class CurlAdapterTest extends \PHPUnit_Framework_TestCase
         $response = $adapter->sendRequest($this->request);
         $this->assertNotNull($response);
     }
-
 }
