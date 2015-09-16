cb8b63548099706d0cb0357590999f1c3512cdd9
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 21:11:50 -0400

    Updated copyright year and minor refactoring

--- src/Bitpay/TokenInterface.php
+++ src/Bitpay/TokenInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License 
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
