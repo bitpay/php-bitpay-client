e33ae9476ab3b8c31e2a122610c14ac11a599561
Rich Morgan <ionux@users.noreply.github.com>
2015-09-16 15:35:41 -0400

    Update MockStorage.php

--- src/Bitpay/Storage/MockStorage.php
+++ src/Bitpay/Storage/MockStorage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @license Copyright 2011-2014 BitPay Inc., MIT License 
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
  * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
  */
 
@@ -8,14 +8,21 @@ namespace Bitpay\Storage;
 
 /**
  * @codeCoverageIgnore
- * @package Bitcore
+ * @package Bitpay
  */
 class MockStorage implements StorageInterface
 {
+    /**
+     * @param Key $key
+     */
     public function persist(\Bitpay\KeyInterface $key)
     {
     }
 
+    /**
+     * @param string $id
+     * @return void
+     */
     public function load($id)
     {
         return;
