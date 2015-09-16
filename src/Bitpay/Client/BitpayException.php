6fc1aa9384b3744b5bfd90c954789bb945baccf0
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 20:58:32 -0400

    Updated copyright year and minor refactoring

--- src/Bitpay/Client/BitpayException.php
+++ src/Bitpay/Client/BitpayException.php
@@ -1,7 +1,14 @@
 <?php 
-
+/**
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
+ * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
+ */
+ 
 namespace Bitpay\Client;
 
+/**
+ * @package Bitpay
+ */
 class BitpayException extends \Exception
 {
 
