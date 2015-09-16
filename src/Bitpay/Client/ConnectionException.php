e2a6fae9ed7c42c9d76374ea05db23df9e243ba9
Rich Morgan <ionux@users.noreply.github.com>
2015-09-15 20:57:49 -0400

    Updated copyright year and minor refactoring

--- src/Bitpay/Client/ConnectionException.php
+++ src/Bitpay/Client/ConnectionException.php
@@ -1,7 +1,13 @@
 <?php 
-
+/**
+ * @license Copyright 2011-2015 BitPay Inc., MIT License
+ * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
+ */
 namespace Bitpay\Client;
 
+/**
+ * @package Bitpay
+ */
 class ConnectionException extends \Exception
 {
 
