5fa09b36e21888139211544c4f2d865a504d54c3
Joshua Lewis <joshlewis@gmail.com>
2019-04-12 11:08:03 -0400

    examples

--- examples/SignPayload.php
+++ examples/SignPayload.php
@@ -1,13 +1,17 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+#require __DIR__.'/../../vendor/autoload.php';
 
 
-$storageEngine = new \Bitpay\Storage\FilesystemStorage();
 
-$private = $storageEngine->load('/tmp/private.key');
-$public  = new \Bitpay\PublicKey('/tmp/public.key');
+#$storageEngine = new \Bitpay\Storage\FilesystemStorage();
+$storageEngine = new \Bitpay\Storage\EncryptedFilesystemStorage('YourTopSecretPassword');
 
+
+$private = $storageEngine->load('/tmp/bitpay.pri');
+$public  = new \Bitpay\PublicKey('/tmp/bitpay.pub');
+error_log('$private '.$private);
 $public->setPrivateKey($private);
 $public->generate();
 
@@ -17,6 +21,5 @@ printf("Private Key: %s\n", $private);
 $message = 'https://test.bitpay.com/subscriptions{"schedule":"weekly","token":"some token","billData":{"currency":"USD","price":"2.00","quantity":1}}';
 
 $signedMessage = $private->sign($message);
-print_r('message to be signed:: ' . $message . "\n");
-print_r('signed message:: ' . $signedMessage);
-
+error_log('message to be signed:: ' . $message . "\n");
+error_log('signed message:: ' . $signedMessage);
---
+++ examples/tutorial/sign.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__.'/../../vendor/autoload.php';
+
+$storageEngine = new \Bitpay\Storage\EncryptedFilesystemStorage('SomeStrongPa$$%wOrD!');
+
+
+$private = $storageEngine->load('/tmp/bitpay.pri');
+$public  = new \Bitpay\PublicKey('/tmp/bitpay.pub');
+#error_log('$private '.$private);
+$public->setPrivateKey($private);
+$public->generate();
+
+$sin    = new \Bitpay\SinKey('/tmp/sin.key');
+$sin->setPublicKey($public);
+$sin->generate();
+
+
+#printf("Public Key:  %s\n", $public);
+#printf("Private Key: %s\n", $private);
+
+#$message = 'https://test.bitpay.com/subscriptions{"schedule":"weekly","token":"some token","billData":{"currency":"USD","price":"2.00","quantity":1}}';
+
+$message = 'https://test.bitpay.com/bills{
+	{
+        "amount":"1.00", 
+        "currency":"USD",
+        "token":"TfCYYmnL9ZArncyeKCFdNX5V8ArudfiUFTp",
+        "description":"netsuite test",
+        "email":"joshlewis@gmail.com"
+    }';
+
+
+$signedMessage = $private->sign($message);
+error_log('$sin ' . $sin);
+error_log('$public ' . $public);
+
+
+error_log('message to be signed:: ' . $message . "\n");
+error_log('signed message:: ' . $signedMessage);
