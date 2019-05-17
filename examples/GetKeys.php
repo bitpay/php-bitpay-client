85b2f349ec3663fdf2eb0ece8187952a30fa8329
Joshua Lewis <joshlewis@gmail.com>
2019-05-17 14:09:36 -0400

    tutorial and library updates Updates made for the tutorials

--- examples/GetKeys.php
+++ examples/GetKeys.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $private = new \Bitpay\PrivateKey();
 //if you've got a hex-encoded private key string, you can use it to create a private key
-$private->setHex('662be90968bc659873d723374213fa5bf7a30c24f0f0713aa798eb7daa7230fc');
+$private->setHex('5555550968bc659873d723374213fa5bf7a30c24f0f0713aa798eb7daa7230fc');
 $public = new \Bitpay\PublicKey();
 $public->generate($private);
 $sin = $public->getSin();
@@ -22,7 +22,7 @@ printf("PEM keypair:  %s\n", $private->pemEncode($keypair));
 
 
 // -or- if you've got a PEM-encoded text file containing your key pair, we can use this
-
+/*
 $keys = file_get_contents(getenv('HOME') . '/.php-bitpay-client/key.pem');
 if (isset($keys) && strlen($keys) > 0) {
     $keys = chop($keys);
@@ -34,3 +34,4 @@ if (isset($keys) && strlen($keys) > 0) {
     printf("Private Key: %s\n", $private);
     printf("Sin Key:     %s\n\n", $private->getPublicKey()->getSin());
 }
+*/
